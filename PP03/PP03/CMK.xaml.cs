e252f08e3bc4c8cfcf6a9fa0d709790797e73457
svr3x <65171515+svr3x@users.noreply.github.com>
2020-12-01 10:13:03 +0300

    Add files via upload
    v 0.0.2

---
+++ PP03/PP03/CMK.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+
+namespace PP03
+{
+    /// <summary>
+    /// Логика взаимодействия для CMK.xaml
+    /// </summary>
+    public partial class CMK : Window
+    {
+        DBProcedures procedures = new DBProcedures();
+        private string QR = "";
+
+        public CMK()
+        {
+            InitializeComponent();
+        }
+
+        private void dgFill(string qr)
+        {
+            Action action = () =>
+            {
+                DBConnection connection = new DBConnection();
+                DBConnection.qrCMK = qr;
+                connection. CMK_Fill();
+                connection.Dependency.OnChange += Dependency_OnChange;
+                dgCMK.ItemsSource = connection.dtCMK.DefaultView;
+                dgCMK.Columns[0].Visibility = Visibility.Collapsed;
+            };
+            Dispatcher.Invoke(action);
+        }
+
+        private void Dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Info != SqlNotificationInfo.Invalid)
+                dgFill(QR);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            QR = DBConnection.qrCMK;
+            dgFill(QR);
+        }
+    
+
+        private void DgCMK_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            switch (e.Column.Header)
+            {
+                case ("Name_CMK"):
+                    e.Column.Header = "Название";
+                    break;
+                
+            }         
+        }
+
+        private void BtCMK_InsertType_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+
+        private void BtCMK_Import_Click(object sender, RoutedEventArgs e)
+        {
+            Action action = () =>
+            {
+                DataGrid dg = dgCMK;
+                dg.SelectAllCells();
+                dg.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
+                ApplicationCommands.Copy.Execute(null, dg);
+                dg.UnselectAllCells();
+                DBProcedures export = new DBProcedures();
+
+                Thread thread = new Thread(export.Export_Word);
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+                thread.Join();
+
+            };
+            Dispatcher.Invoke(action);
+
+
+
+            //Excel.Application exApp = new Excel.Application();
+            //exApp.Workbooks.Add();
+            //Excel.Worksheet wsh = (Excel.Worksheet)exApp.ActiveSheet;
+            //int i, j;
+            //for (i = 0; i <= dgCMK.Columns.Count - 2; i++)
+            //{
+            //    for (j = 0; j <= dgCMK.Columns.Count - 1; j++)
+            //    {
+            //        wsh.Cells[i + 1, j + 1] = dgCMK[j, i].Value.ToString();
+            //    }
+            //}
+            //exApp.Visible = true;
+
+
+        }
+
+        private void BtClose_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow MainWindow = new MainWindow();
+            MainWindow.Show();
+            Visibility = Visibility.Collapsed;
+        }
+    }
+}
---
+++ PP03/PP03/Configuration_Class.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP03
+{
+    class Configuration_Class
+    {
+        public event  Action<DataTable> server_Collection;
+        public event  Action<DataTable> Data_Base_Collection;
+        public event  Action<bool> connection_checked;
+
+        public string DS = "Empty", IC = "Empty";
+        public string ds = "";
+
+
+        public static SqlConnection connection = new SqlConnection();
+
+        public void SQL_Server_Configuration_get()
+        {
+            RegistryKey registry = Registry.CurrentUser;
+            RegistryKey key = registry.CreateSubKey("Server_Configuration");
+            try
+            {
+                DS = key.GetValue("DS").ToString();
+                IC = key.GetValue("IC").ToString();
+            }
+            catch
+            {
+                DS = "Empty";
+                IC = "Empty";
+            }
+            finally
+            {
+                connection.ConnectionString = "Data Source = " + DS + "; Initial Catalog = " + IC + "; Integrated Security = true;";
+
+            }
+        }
+
+        public void SQL_Server_Configuration_Set(string ds, string ic)
+        {
+            RegistryKey registry = Registry.CurrentUser;
+            RegistryKey key = registry.CreateSubKey("Server_Configuration");
+            key.SetValue("DS", ds);//Запись значения в переменную реестра
+            key.SetValue("IC", ic);
+            SQL_Server_Configuration_get();
+        }
+
+        public void SQL_Server_Enumurator()
+        {
+            SqlDataSourceEnumerator sourceEnumerator = SqlDataSourceEnumerator.Instance;
+            server_Collection(sourceEnumerator.GetDataSources());
+        }
+
+        public void SQL_Data_Base_Checking()
+        {
+            connection.ConnectionString = "Data source = " + ds + "; " + "Initial Catalog = master; Integrated Security = true";
+            try
+            {
+                connection.Open();
+                connection_checked(true);
+            }
+            catch
+
+            {
+                connection_checked(false);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+
+        public void SQL_Data_Base_Collection()
+        {
+            SqlCommand command = new SqlCommand("select name from sys.databases " + "where name not in ('master','tempdb','model','msdb')", connection);
+            try
+            {
+                connection.Open();
+                DataTable table = new DataTable();
+                table.Load(command.ExecuteReader());
+                Data_Base_Collection(table);
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
